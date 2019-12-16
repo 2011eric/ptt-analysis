eb0527236ec787f81b6496736396d0463e372865
William Chang <wilicw1346@gmail.com>
2019-12-16 22:48:00 +0800

    redesign and highlight comments

--- web/cli/src/components/Page.vue
+++ web/cli/src/components/Page.vue
@@ -1,70 +1,74 @@
 <template>
 <v-container>
-  <div class="d-flex justify-center mb-6 flex-column text-center">
-    <v-card class="pa-5 my-5 text-left">
-      <p class="title">過濾條件</p>
-      <v-text-field v-model="search"></v-text-field>
+  <v-row no-gutters>
 
-      <div class="d-inline-flex flex-column ma-3">
-        <p class="text-left">日期</p>
-        <v-menu v-model="smenu" :close-on-content-click="false" :nudge-right="80" transition="scale-transition" offset-y>
-          <template v-slot:activator="{ on }">
-            <v-text-field v-model="sdate" label="開始日期" prepend-icon="mdi-calendar-clock" readonly v-on="on"></v-text-field>
-          </template>
-          <v-date-picker v-model="sdate" @input="smenu = false"></v-date-picker>
-        </v-menu>
-        <v-menu v-model="emenu" :close-on-content-click="false" :nudge-right="80" transition="scale-transition" offset-y>
-          <template v-slot:activator="{ on }">
-            <v-text-field v-model="edate" label="結束日期" prepend-icon="mdi-calendar-clock" readonly v-on="on"></v-text-field>
-          </template>
-          <v-date-picker v-model="edate" @input="emenu = false"></v-date-picker>
-        </v-menu>
-      </div>
-      <div class="d-inline-flex flex-column ma-3">
-        <p>資料數: {{total.length}}</p>
-      </div>
-      <br>
-      <v-divider></v-divider>
-      <br>
-      <div class="d-flex flex-row">
-        <v-spacer></v-spacer>
-        <v-btn @click="cleanFilter" width="80px" class="mx-5">清除條件</v-btn>
-        <v-btn @click="searchPost" color="primary" width="80px">搜尋</v-btn>
-      </div>
-    </v-card>
-    <br>
-    <br>
-    <div v-if="show.length==0">
-      <v-progress-circular :size="80" :width="3" class="ma-5 pa-5" color="primary" indeterminate></v-progress-circular>
-    </div>
-    <div v-for="item in show" :key="item.id">
-      <v-card class="mx-auto text-left">
-        <v-card-text>
-          <div>{{ item.board }}</div>
-          <p class="display-1 text--primary">{{ item.title }}</p>
-          <p>{{ new Date(item.timestamp) }}</p>
-          <p>{{ item.author }}</p>
-          <div class="text--primary" v-html="item.content"></div>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn text color="deep-purple accent-4" @click="getcomments(item.id)">留言</v-btn>
-          <v-btn text color="deep-purple accent-4" @click="go(item.link)">原文連結</v-btn>
-        </v-card-actions>
-        <div v-if="item.showComment">
-          <div v-for="comment in item.comments" :key="comment.id">
-            <v-card-subtitle class="py-0">{{ comment.user }} ({{ comment.ip }})</v-card-subtitle>
-            <v-card-text class="text--primary"><span class="blue-grey--text">{{ comment.tag }}</span> {{ comment.text }}</v-card-text>
-          </div>
+    <v-col xs="12" md="4" class="px-5">
+      <v-card class="pa-5 my-5 text-left">
+        <p class="title">過濾條件</p>
+        <v-text-field v-model="search"></v-text-field>
+        <div class="d-inline-flex flex-column ma-3">
+          <p class="text-left">日期</p>
+          <v-menu v-model="smenu" :close-on-content-click="false" :nudge-right="80" transition="scale-transition" offset-y>
+            <template v-slot:activator="{ on }">
+              <v-text-field v-model="sdate" label="開始日期" prepend-icon="mdi-calendar-clock" readonly v-on="on"></v-text-field>
+            </template>
+            <v-date-picker v-model="sdate" @input="smenu = false"></v-date-picker>
+          </v-menu>
+          <v-menu v-model="emenu" :close-on-content-click="false" :nudge-right="80" transition="scale-transition" offset-y>
+            <template v-slot:activator="{ on }">
+              <v-text-field v-model="edate" label="結束日期" prepend-icon="mdi-calendar-clock" readonly v-on="on"></v-text-field>
+            </template>
+            <v-date-picker v-model="edate" @input="emenu = false"></v-date-picker>
+          </v-menu>
+        </div>
+        <div class="d-inline-flex flex-column ma-3">
+          <p>資料數: {{total.length}}</p>
+        </div>
+        <br>
+        <v-divider></v-divider>
+        <br>
+        <div class="d-flex flex-row">
+          <v-spacer></v-spacer>
+          <v-btn @click="cleanFilter" width="80px" class="mx-5">清除條件</v-btn>
+          <v-btn @click="searchPost" color="primary" width="80px">搜尋</v-btn>
         </div>
       </v-card>
-      <br>
-    </div>
-  </div>
-  <div v-if="barlength > 1 && show.length!=0" class="pa-5 mb-5">
-    <div class="text-center">
-      <v-pagination v-model="page" @input="render()" :length="barlength" :total-visible="11"></v-pagination>
-    </div>
-  </div>
+    </v-col>
+
+    <v-col xs="12" md="8" class="py-5">
+      <div v-if="show.length==0">
+        <v-progress-circular :size="80" :width="3" class="ma-5 pa-5" color="primary" indeterminate></v-progress-circular>
+      </div>
+      <div v-for="item in show" :key="item.id">
+        <v-card class="mx-auto text-left" width="100%">
+          <v-card-text>
+            <div>{{ item.board }}</div>
+            <p class="display-1 text--primary">{{ item.title }}</p>
+            <p>{{ new Date(item.timestamp) }}</p>
+            <p>{{ item.author }}</p>
+            <div class="text--primary" v-html="item.content"></div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text color="deep-purple accent-4" @click="getcomments(item.id)">留言</v-btn>
+            <v-btn text color="deep-purple accent-4" @click="go(item.link)">原文連結</v-btn>
+          </v-card-actions>
+          <div v-if="item.showComment">
+            <div v-for="comment in item.comments" :key="comment.id">
+              <v-card-subtitle class="py-0">{{ comment.user }} ({{ comment.ip }})</v-card-subtitle>
+              <v-card-text class="text--primary"><span class="blue-grey--text">{{ comment.tag }}</span> <span v-html="comment.text"></span></v-card-text>
+            </div>
+          </div>
+        </v-card>
+        <br>
+      </div>
+      <div v-if="barlength > 1 && show.length!=0" class="pa-5 mb-5">
+        <div class="text-center">
+          <v-pagination v-model="page" @input="render()" :length="barlength" :total-visible="11"></v-pagination>
+        </div>
+      </div>
+    </v-col>
+  </v-row>
+
 </v-container>
 </template>
 
@@ -86,7 +90,8 @@ export default {
     edate: null,
     smenu: false,
     emenu: false,
-    menu: false
+    menu: false,
+    showComment: false
   }),
   beforeMount: function() {
     this.load()
@@ -94,6 +99,7 @@ export default {
   methods: {
     load: function() {
       let self = this
+      self.showComment = false
       if (process.env.NODE_ENV == 'development') {
         self.api = 'http://localhost:8081/api'
       } else {
@@ -135,7 +141,15 @@ export default {
       axios.get(`${self.api}/get/post/${arr}`).then(res => {
         res.data.forEach(o => {
           o['comments'] = []
-          o['showComment'] = false
+          o['showComment'] = self.showComment
+          if (self.showComment) {
+            axios.get(`${self.api}/get/comments/${o.id}`).then(res => {
+              o.comments = res.data
+              o.comments.forEach((c) => {
+                c.text = c.text.replace(self.search, `<span class="light-green lighten-2">${self.search}</span>`)
+              })
+            })
+          }
           o.content = o.content.replace(/\n/g, '<br>')
         })
         self.show = res.data
@@ -172,12 +186,15 @@ export default {
         })
         self.page = 1
         self.show = []
+        self.showComment = true
         self.render()
       })
     },
     cleanFilter: function() {
       this.sdate = null
       this.edate = null
+      this.search = ""
+      this.load()
     }
   }
 }
